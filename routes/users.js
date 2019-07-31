ece9160fc6490d888082202b08448cbce3a51281
yohan yoo <public.afant@gmail.com>
2019-07-31 18:00:46 +0900

    TEST Path URL Params "/users/:user_id"

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const controllers = require("../controllers");
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get("/", function(req, res, next) {
+  res.send("All Users Request.");
+});
+router.get("/:user_id", function(req, res, next) {
+  console.log(req.params);
+  res.send(`users/${req.params.user_id} Users Request.`);
+});
 router.get("/test", controllers.users.test.get);
 
 module.exports = router;
