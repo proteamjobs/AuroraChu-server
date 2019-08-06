779efad18aec1ccf574c82b73c7ea4e28f6de331
yohan yoo <public.afant@gmail.com>
2019-08-06 18:31:20 +0900

    ADD /auth/verify API

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ let controllers = require("../controllers");
 /* GET home page. */
 router.post("/login", controllers.auth.login.post);
 router.get("/me", controllers.auth.me.get);
+router.get("/verify", controllers.auth.verify.get);
 
 module.exports = router;
