f36cc0bde355e6f7c9dd1964c8caf39a72e6ce38
yohan yoo <public.afant@gmail.com>
2019-08-19 09:10:12 +0900

    /videos/:video_id/complete Complete

--- routes/videos.js
+++ routes/videos.js
@@ -4,6 +4,7 @@ let controllers = require("../controllers");
 
 /* GET home page. */
 router.get("/", controllers.videos.get);
+router.post("/:video_id/complete", controllers.videos.complete.post);
 // router.get("/me", controllers.auth.me.get);
 
 module.exports = router;
