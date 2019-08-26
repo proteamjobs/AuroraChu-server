41be546c069bdebe13435d490a63beb35e2475eb
yohan yoo <public.afant@gmail.com>
2019-08-26 12:03:35 +0900

    Complete /applies/delete

--- routes/applies.js
+++ routes/applies.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controllers = require("../controllers");
 
 router.get("/status", controllers.applies.status.get);
+router.delete("/", controllers.applies.delete);
 
 module.exports = router;
