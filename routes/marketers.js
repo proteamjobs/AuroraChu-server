171da6ad01baa5bef7d74563dff39deb7a7f434d
yohan yoo <public.afant@gmail.com>
2019-09-09 18:00:14 +0900

    Fixed router rank

--- routes/marketers.js
+++ routes/marketers.js
@@ -5,12 +5,12 @@ const controllers = require("../controllers");
 router.get("/", controllers.marketers.get);
 router.post("/", controllers.marketers.post);
 // router.get("/:post_id")
+router.get("/latest", controllers.marketers.latest.get);
 
 router.get("/@:nickname", controllers.marketers.nickname.get);
 router.get("/:category", (req, res) => {
   res.send("GET /#:category");
 });
-router.get("/latest", controllers.marketers.latest.get);
 router.get("/test", controllers.marketers.test.get);
 
 module.exports = router;
